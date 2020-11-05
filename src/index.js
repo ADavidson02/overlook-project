a8c810ad4d1a02c714ff056505950e93e8aab107
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-11-04 18:37:14 -0700

    Add styling for landing pages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './css/manager-view.scss';
+import './css/guest-view.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './index.html'
--- test/test-data.js
+++ test/test-data.js
@@ -8,7 +8,8 @@ const roomData = [
 const userData = [
   {"id":1,"name":"Leatha Ullrich"},
   {"id":2,"name":"Rocio Schuster"},
-  {"id":3,"name":"Kelvin Schiller"},{"id":4,"name":"Kennedi Emard"}
+  {"id":3,"name":"Kelvin Schiller"},
+  {"id":4,"name":"Kennedi Emard"},
 ]
 
 const bookingsData = [
